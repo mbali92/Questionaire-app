d11630a107744ee45e67842bf670fd243f20c922
mbali92 <83025186+mbali92@users.noreply.github.com>
2023-08-31 13:25:59 +0200

    changing file name

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Questionlist from './Questionlist'
-import Questchecks from './questchecks'
+import Questchecks from './Questchecks'
 import Dashboard from './Dashboard'
 
 function Context({componentName}) {
